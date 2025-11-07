6628ae25d1b8330c5634ad989ab09847c47ed629
lea-jh8 <hsinlok07@gmail.com>
2025-11-07 12:08:41 +0800

    Add

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -15,7 +15,9 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
         System.out.println("test");
+        System.out.println("Hello2");
     }
 
 
