64dc962bec80c4f2fb47dd3396365c9392b16891
lea-jh8 <hsinlok07@gmail.com>
2025-11-07 11:58:16 +0800

    update print test

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -15,7 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        System.out.println("test");
     }
 
-    System.out.println("Hello");
+
 }
